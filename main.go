8438204be133f32c910c586b1fec0f66c8c6e230
NoobAuthor <igor21.assis@outlook.com>
2024-08-10 11:15:37 -0400

    Main structure

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: go run main.go <url>")
+	}
+
+	url := os.Args[1]
+
+	utils.InitLogger()
+
+	links, err := scraper.FetchAndParse(url)
+	if err != nil {
+		log.Fatalf("Failed to fetch and parse the page: %v", err)
+	}
+
+	deadLinks := scraper.CheckLinks(links)
+
+	if len(deadLinks) == 0 {
+		fmt.Println("No dead links found")
+	} else {
+		fmt.Println("Dead links foung:")
+		for _, link := range deadLinks {
+			fmt.Println(link)
+		}
+	}
+}
