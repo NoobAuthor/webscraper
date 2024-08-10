83801eba8a61de4e3acf9ddb0c7b3df18d38aed3
NoobAuthor <igor21.assis@outlook.com>
2024-08-10 15:03:47 -0400

    Logger ready

---
+++ utils/logger.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"log"
+	"os"
+)
+
+func InitLogger() {
+	logFile, err := os.OpenFile("webscraper.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open log file:", err)
+	}
+
+	log.SetOutput(logFile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
